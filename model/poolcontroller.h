5cc65e323a58864f7c818f026614edef9d9d3eca
Aleksandr Ploskov <merhalak@gmail.com>
2017-05-21 23:28:54 +0300

    Добавил примерное описание шагов вычисления в PoolController.

--- model/poolcontroller.h
+++ model/poolcontroller.h
@@ -1,11 +1,14 @@
 #ifndef POOLCONTROLLER_H
 #define POOLCONTROLLER_H
 
+class Table;
 
 class PoolController
 {
 public:
     PoolController();
+
+    void calculateHit();
 };
 
 #endif // POOLCONTROLLER_H
