a6a9ab04c60cc3410ffbc70b09f8330d80092d54
Aleksandr Ploskov <merhalak@gmail.com>
2017-05-21 18:23:39 +0300

    Добавлена возможность добавления шаров на стол. Необходимо реализовать проверку коллизии между двумя шарами, прежде чем реализовывать метод добавления. Добавлен деструктор Table для удаления объектов шаров.

--- model/objects/table.cpp
+++ model/objects/table.cpp
@@ -1,4 +1,7 @@
 #include "table.h"
+#include "ball.h"
+
+#include <algorithm>
 
 Table::Table(double width,
              double height)
@@ -14,4 +17,22 @@ double Table::getWidth() const
 double Table::getHeight() const
 {
     return height;
+}
+
+bool Table::addBall(Ball * /*ball*/)
+{
+
+    // TODO: Подготовить добавление шара на стол, учитывая коллизии с другими
+    // шарами
+
+    return false;
+}
+
+Table::~Table()
+{
+    std::for_each(balls.begin(),
+                  balls.end(),
+                  [](Ball * ball) {
+        delete ball;
+    });
 }
--- model/objects/table.h
+++ model/objects/table.h
@@ -1,6 +1,9 @@
 #ifndef TABLE_H
 #define TABLE_H
 
+#include <set>
+
+class Ball;
 
 class Table
 {
@@ -8,12 +11,18 @@ private:
     double const width;
     double const height;
 
+    std::set<Ball *> balls;
+
 public:
     Table(double width,
           double height);
 
     double getWidth() const;
     double getHeight() const;
+
+    bool addBall(Ball * ball);
+
+    ~Table();
 };
 
 #endif // TABLE_H
